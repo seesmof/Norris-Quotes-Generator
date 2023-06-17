de0dabf61c54aa2a790a6ed5a111bf19fa8663bb
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-17 08:26:16 +0300

    feat: initial project setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# hello world with react
- 
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-slate-900 text-slate-50 w-full">
+        <div className="p-6 flex container mx-auto max-w-4xl flex-col">
+          <h1 className="font-medium text-4xl">Hello World</h1>
+
+          <button className="button mt-4">Click Me</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
