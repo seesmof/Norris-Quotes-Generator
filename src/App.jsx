197a354ce5cba790dcfbb85d930ad199e781c728
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-17 10:19:39 +0300

    feat: make it responsive, add image on larger screens

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,31 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-slate-900 text-slate-50 w-full">
-        <div className="p-6 flex container mx-auto max-w-4xl flex-col">
-          <h1 className="font-medium text-4xl">Hello World</h1>
+      <div className="min-h-screen bg-slate-900 text-slate-50 w-full flex items-center justify-center">
+        <div className="flex flex-row gap-12 p-6 container mx-auto max-w-5xl">
+          <div className="flex flex-col flex-1">
+            <h1 className="font-medium text-4xl border-b pb-4 border-slate-400">
+              Chuck Norris Quotes
+            </h1>
+            <p className="text-lg pt-4">
+              This is a simple application that uses Chuck Norris API to get a
+              random joke. All you have to do is to click the button below.
+            </p>
 
-          <button className="button my-4" onClick={updateJoke}>
-            Click Me
-          </button>
+            <button className="button capitalize my-4" onClick={updateJoke}>
+              generate joke
+            </button>
 
-          <ChuckQuote joke={joke} />
+            <ChuckQuote joke={joke} />
+          </div>
+
+          <div className="hidden max-w-sm lg:flex">
+            <img
+              src="/chuck.jpg"
+              alt="Chuck Norris portrait image"
+              className="rounded-md"
+            />
+          </div>
         </div>
       </div>
     </>
