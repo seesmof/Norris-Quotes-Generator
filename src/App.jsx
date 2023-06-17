5f2c7fcf3469541ee8552e623063581cecab2c86
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-17 05:59:08 +0000

    🌿: Saturday, June 17, 2023 at 8:59:08 AM Eastern European Summer Time

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import React, { useEffect, useState } from "react";
 import ChuckQuote from "./components/ChuckQuote";
 
 function App() {
+  const addJoke = () => {
+    const jokesContainer = document.getElementById("jokes-container");
+    const newJoke = document.createElement("div");
+    newJoke.innerHTML = ChuckQuote();
+    jokesContainer.appendChild(newJoke);
+  };
+
   return (
     <>
       <div className="min-h-screen bg-slate-900 text-slate-50 w-full">
         <div className="p-6 flex container mx-auto max-w-4xl flex-col">
           <h1 className="font-medium text-4xl">Hello World</h1>
 
-          <button className="button mt-4">Click Me</button>
+          <button className="button my-4" onClick={addJoke}>
+            Click Me
+          </button>
 
-          <ChuckQuote />
+          <div className="flex flex-col gap-4" id="jokes-container"></div>
         </div>
       </div>
     </>
--- src/components/ChuckQuote.jsx
+++ src/components/ChuckQuote.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 const ChuckQuote = () => {
-  const fetchJoke = async () => {
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
-    const data = await response.json();
-    return data;
-  };
-
   const [joke, setJoke] = useState("");
 
   useEffect(() => {
+    const fetchJoke = async () => {
+      const response = await fetch("https://api.chucknorris.io/jokes/random");
+      const data = await response.json();
+      return data;
+    };
+
     fetchJoke().then((data) => {
       setJoke(data.value);
     });
   }, []);
 
-  console.log(joke);
-
   return (
     <>
-      <div className="fle"></div>
+      <div className="flex flex-col gap-1">
+        <blockquote className="border-l-2 border-slate-300 pl-4">
+          {joke}
+        </blockquote>
+        <cite className="self-end">- Chuck Norris</cite>
+      </div>
     </>
   );
 };
