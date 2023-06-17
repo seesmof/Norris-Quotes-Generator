a38cc6fcf767305e51d256c97ccf545fe8404029
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-17 10:28:40 +0300

    refactor: remove unnecessary dependancies from components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ChuckQuote from "./components/ChuckQuote";
 import ChuckPhoto from "/chuck.jpg";
 
